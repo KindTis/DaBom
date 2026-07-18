0bb2df5bfb4074dabd39cf21e78aec0e87530a31
KindTis <tatis3@gmail.com>
2026-07-19 02:18:50 +0900

    feat: polish reference-aligned main window

--- tests/Dabom.Tests/MainWindowMarkupTests.cs
+++ tests/Dabom.Tests/MainWindowMarkupTests.cs
@@ -24,6 +24,42 @@ public sealed class MainWindowMarkupTests
         StringAssert.Contains(markup, "ScrollViewer.VerticalScrollBarVisibility=\"Auto\"");
     }
 
+    [TestMethod]
+    public void FeaturedPlay_UsesReferencePrimaryAction()
+    {
+        var markup = ReadMainWindowMarkup();
+
+        StringAssert.Contains(markup, "x:Key=\"PrimaryActionButtonStyle\"");
+        StringAssert.Contains(markup, "Style=\"{StaticResource PrimaryActionButtonStyle}\"");
+        StringAssert.Contains(markup, "Data=\"M 0 0 L 8 5 L 0 10 Z\"");
+        StringAssert.Contains(markup, "AutomationProperties.Name=\"재생하기\"");
+    }
+
+    [TestMethod]
+    public void LibraryToolbar_ContainsSearchGuidanceAndSortLabel()
+    {
+        var markup = ReadMainWindowMarkup();
+
+        StringAssert.Contains(markup, "x:Name=\"LibraryToolbar\"");
+        StringAssert.Contains(markup, "Text=\"제목, 감독, 배우 이름으로 검색\"");
+        StringAssert.Contains(markup, "Text=\"Ctrl K\"");
+        StringAssert.Contains(markup, "Text=\"정렬\"");
+        StringAssert.Contains(markup, "AutomationProperties.Name=\"영상 검색\"");
+        StringAssert.Contains(markup, "AutomationProperties.Name=\"정렬\"");
+    }
+
+    [TestMethod]
+    public void CardPopup_ShowsPosterAndReferenceMetadata()
+    {
+        var markup = ReadMainWindowMarkup();
+
+        StringAssert.Contains(markup, "<ColumnDefinition Width=\"126\" />");
+        StringAssert.Contains(markup, "Image Source=\"{Binding Poster}\"");
+        StringAssert.Contains(markup, "Text=\"상영일\"");
+        StringAssert.Contains(markup, "Text=\"주요 배우\"");
+        StringAssert.Contains(markup, "Text=\"영상\"");
+    }
+
     private static string ReadMainWindowMarkup()
     {
         var path = Path.GetFullPath(Path.Combine(
